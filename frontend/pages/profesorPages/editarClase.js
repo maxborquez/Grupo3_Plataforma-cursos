8f79ed7451a50a5dedb2ba27b6a0a634d18707c4
Max-Borquez <max.borquezc@gmail.com>
2023-07-29 00:28:16 -0400

    correcciones editarClase

--- frontend/pages/profesorPages/editarClase.js
+++ frontend/pages/profesorPages/editarClase.js
@@ -1,62 +1,60 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { Box, Heading, FormControl, FormLabel, Input, Button, VStack } from "@chakra-ui/react";
-import { obtenerClasePorId, actualizarClase } from "../../data/clasesData";
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import {
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  VStack,
+} from '@chakra-ui/react';
+import { obtenerClasePorId, actualizarClase } from '../../data/clasesData';
 
 const EditarClase = () => {
   const router = useRouter();
   const { claseId } = router.query;
-  const [clase, setClase] = useState(null);
-  const [nombre, setNombre] = useState("");
-  const [fecha, setFecha] = useState("");
 
-  // Cargar la clase al cargar la página
+  // Estado para almacenar los detalles de la clase
+  const [clase, setClase] = useState({}); // Inicializa como un objeto vacío {}
+
+  // Carga los detalles de la clase al cargar la página
   useEffect(() => {
     if (claseId) {
-      loadClase();
+      obtenerClase();
     }
   }, [claseId]);
 
-  // Función para cargar la clase desde el servidor
-  const loadClase = async () => {
+  // Función para obtener los detalles de la clase por su ID
+  const obtenerClase = async () => {
     try {
-      const response = await obtenerClasePorId(claseId);
-      if (response.state === "Success") {
-        setClase(response.data);
-        setNombre(response.data.nombre);
-        setFecha(response.data.fecha);
-      } else {
-        console.error("Error al obtener la clase:", response);
-      }
+      const claseObtenida = await obtenerClasePorId(claseId);
+      setClase(claseObtenida);
     } catch (error) {
-      console.error("Error al cargar la clase:", error);
+      console.error('Error al obtener la clase:', error);
     }
   };
 
-  // Función para manejar el envío del formulario
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  // Función para manejar el cambio en los campos del formulario
+  const handleChange = (e) => {
+    setClase({
+      ...clase,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Función para manejar el envío del formulario de edición
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      // Realizar la actualización de la clase
-      const updatedClase = {
-        nombre: nombre,
-        fecha: fecha,
-      };
-      const response = await actualizarClase(claseId, updatedClase);
-      if (response.state === "Success") {
-        // Regresar a la página anterior (detalles del curso)
-        router.back();
-      } else {
-        console.error("Error al actualizar la clase:", response);
-      }
+      // Envía la solicitud para actualizar la clase
+      await actualizarClase(claseId, clase);
+      // Redirige a la página anterior después de la edición exitosa
+      router.back();
     } catch (error) {
-      console.error("Error al actualizar la clase:", error);
+      console.error('Error al editar la clase:', error);
     }
   };
-  
-  if (!clase) {
-    return <div>Cargando...</div>;
-  }
 
   return (
     <Box mt={4} p={4} borderWidth="1px" borderRadius="md">
@@ -66,25 +64,27 @@ const EditarClase = () => {
       <form onSubmit={handleSubmit}>
         <VStack align="start" spacing={4}>
           <FormControl>
-            <FormLabel>Nombre de la clase</FormLabel>
+            <FormLabel>Nombre de la Clase:</FormLabel>
             <Input
-              type="text"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
+              name="nombre"
+              value={clase.nombre || ''}
+              onChange={handleChange}
               required
             />
           </FormControl>
           <FormControl>
-            <FormLabel>Fecha de la clase</FormLabel>
+            <FormLabel>Fecha de la Clase:</FormLabel>
             <Input
               type="date"
-              value={fecha}
-              onChange={(e) => setFecha(e.target.value)}
+              name="fecha"
+              value={clase.fecha || ''}
+              onChange={handleChange}
               required
             />
           </FormControl>
+          {/* Puedes agregar más campos de la clase aquí según tu modelo */}
           <Button colorScheme="blue" type="submit">
-            Guardar cambios
+            Guardar Cambios
           </Button>
         </VStack>
       </form>
