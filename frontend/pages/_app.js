1997860adb73c07445053005194f214b3940ef6a
Max-Borquez <max.borquezc@gmail.com>
2023-07-29 01:09:32 -0400

    colores

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import dotenv from 'dotenv';
+import chakraTheme from '../styles/chakra-theme'; // Asegúrate de usar la ruta correcta hacia tu chakra-theme.js
 
 dotenv.config();
 
 function MyApp({ Component, pageProps }) {
-  const theme = extendTheme({
-    fonts: {
-      heading: 'Baloo Bhai, sans-serif',
-      body: 'Baloo Bhai, sans-serif',
-    },
+  const combinedTheme = extendTheme(chakraTheme, {
+    // Puedes definir más estilos específicos de la aplicación aquí si lo deseas
   });
 
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={combinedTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,23 +3,23 @@ import { Box, Button, Heading, VStack, Text } from '@chakra-ui/react';
 
 const IndexPage = () => {
   return (
-    <Box p={4} bg="gray.100" minHeight="100vh">
+    <Box p={4} bg="NegroPri" minHeight="100vh"> {/* Usamos el color primaryBlack de la paleta */}
       <VStack spacing={8} align="center">
         <br/>
         <br/>
-        <Heading as="h1" size="2xl" textAlign="center" color="teal.500">
+        <Heading as="h1" size="2xl" textAlign="center" color="Naranja">
           Centro de Formación Tech
         </Heading>
-        <Text fontSize="xl" textAlign="center" color="gray.600">
+        <Text fontSize="xl" textAlign="center" color="Blanco">
           Aprende y crece con nosotros.
         </Text>
         <Link href="/login">
-          <Button colorScheme="teal" size="lg">
+          <Button colorScheme="Naranja" size="lg"> {/* Usamos el color naranjo de la paleta */}
             Iniciar Sesión
           </Button>
         </Link>
         <Link href="/signup">
-          <Button colorScheme="blue" size="lg">
+          <Button colorScheme="Cafe" size="lg"> {/* Usamos el color café de la paleta */}
             Registrarse
           </Button>
         </Link>
